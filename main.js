c2a27d3dbccc9bf750c45fa31812e5bc24aa0526
bbouley <b.bouley@hotmail.co.uk>
2015-07-22 14:23:47 -0600

    added functions to get student names

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-console.log('testing')
+function student(name, number, city){
+  this.name = prompt('Student Name:');
+  this.number = prompt('Student Number:');
+  this.city = prompt('Student city');
+}
+
+function createStudent(numOfStudents){
+  var studentArray = [];
+  for (var i = 0; i < numOfStudents; i++) {
+  var studentDetails = new student;
+    studentArray.push(studentDetails);
+  } console.log(studentArray);
+};
+
+createStudent(prompt('How many students?'));
+
+
+
+
+
+
