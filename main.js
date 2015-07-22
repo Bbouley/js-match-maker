02553b9cd9c2854570afbd48e239a7392499e0c8
bbouley <b.bouley@hotmail.co.uk>
2015-07-22 15:09:22 -0600

    alert function for student names completed

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+var studentArray = createStudent(prompt('How many students?'));
+
+var mentorArray = createMentor(prompt('How many mentors?'));
+
 function student(name, number, city){
   this.name = prompt('Student Name:');
   this.number = prompt('Student Number:');
@@ -9,10 +13,38 @@ function createStudent(numOfStudents){
   for (var i = 0; i < numOfStudents; i++) {
   var studentDetails = new student;
     studentArray.push(studentDetails);
-  } console.log(studentArray);
-};
+  } return studentArray;
+}
 
-createStudent(prompt('How many students?'));
+function mentor(name, number, city){
+  this.name = prompt('Mentor Name:');
+  this.number = prompt('Mentor Number:');
+  this.city = prompt('Mentor city');
+}
+
+function createMentor(numOfMentors){
+  var mentorArray = [];
+  for (var i = 0; i < numOfMentors; i++) {
+  var mentorDetails = new mentor;
+    mentorArray.push(mentorDetails);
+  } return mentorArray;
+}
+
+var numOfStudents = studentArray.length;
+
+var numOfMentors = mentorArray.length;
+
+function printStudents(array){
+  for (var i = 0; i<array.length; i++) {
+  alert('Name : ' + array[i].name + '\n' +
+        'Phone Number : ' + array[i].number + '\n' +
+        'City : ' + array[i].city);
+  };
+}
+
+printStudents(studentArray);
+
+
 
 
 
